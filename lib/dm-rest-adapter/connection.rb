2c3a8ab086644c322834c278cc88b628e16f2730
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-08-20 17:20:14 -0400

    [ARCH] Extracted a simple connection code using Typhoeus and get rid of Net::HTTP

--- lib/dm-rest-adapter/connection.rb
+++ lib/dm-rest-adapter/connection.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'typhoeus'
 
 begin
   require 'active_support/inflector'
@@ -10,82 +10,98 @@ rescue LoadError
 end
 
 module DataMapperRest
-  # Somewhat stolen from ActiveResource
-  # TODO: Support https?
+  # Extracted from private code
   class Connection
-    attr_accessor :uri, :format
 
-    def initialize(uri, format)
-      @uri = uri
-      @format = Format.new(format)
+    attr_reader :environment, :connection_options, :auth_token 
+    class_inheritable_accessor :default_connection
+
+    def initialize(options = {})
+      @environment = options[:environment] || Rails.env
+      @connection_options = ACCESS_CREDENTIALS[@environment].merge(options)
     end
 
-    # this is used to run the http verbs like http_post, http_put, http_delete etc.
-    # TODO: handle nested resources, see prefix in ActiveResource
-    def method_missing(method, *args)
-      if verb = method.to_s.match(/\Ahttp_(get|post|put|delete|head)\z/)
+    # Replace this with the URI library?
+    def base_url
+      @base_url ||= [
+        (ssl? ? 'https://' : 'http://' ), 
+        connection_options[:host],
+        ':' + (connection_options[:port].to_s || (ssl? ? '443' : '80'))].join
+    end
 
-        orig_uri, @uri = @uri, @uri.dup
-        begin
-          path, query = args[0].split('?', 2)
-          @uri.path = "#{path}.#{@format.extension}#{'?' << query if query}" # Should be the form of /resources
-          run_verb(verb.to_s.split('_').last, args[1])
-        ensure
-          @uri = orig_uri
+    def ssl?
+      connection_options[:ssl]
+    end
+
+    def authenticate!
+      true
+    end
+
+    # TODO: Add :payload that intelligently figure out GET or POST requests
+    # TODO: Add auto-reauthenticate, probably have to put it in a thread-safe queue or something
+    def request(method, path, options = {})
+      authenticate! unless self.auth_token
+      request = {
+        :headers => { 
+        'X-Auth-Token' => self.auth_token ,
+        # TODO: Fix this hard-coded JSON mime-type
+        'Content-Type' => Mime::JSON.to_s,}.merge(options[:headers] || {}), 
+        :method  => method }
+
+        # See O'Reilly The Ruby Programming Language, p 126
+        case method
+        when :post, :put
+          request.merge!(:body => options[:payload])
+        else
         end
+
+        handle_response(Typhoeus::Request.run("#{self.base_url}/#{path}", request))
+    end
+
+    [:head, :get, :post, :put, :delete].each do |method|
+      define_method("http_#{method}") do |path, options|
+        self.request(method, path, options || {})
       end
     end
 
-    protected
+    # Convenience methods
+    def self.set_default_connection(connection)
+      Connection.default_connection = connection
+    end
 
-      def run_verb(verb, data = nil)
-        request do |http|
-          klass = Net::HTTP.find_const(ActiveSupport::Inflector.camelize(verb))
-          request = klass.new(@uri.to_s, @format.header)
-          request.basic_auth(@uri.user, @uri.password) if @uri.user && @uri.password
-          result = http.request(request, data)
+    def self.connection
+      Connection.default_connection
+    end
 
-          handle_response(result)
-        end
+    # Handles response and error codes from remote service.
+    def handle_response(response)
+      case response.code.to_i
+      when 301,302
+        raise(Redirection.new(response))
+      when 200...400
+        response
+      when 400
+        raise(BadRequest.new(response))
+      when 401
+        raise(UnauthorizedAccess.new(response))
+      when 403
+        raise(ForbiddenAccess.new(response))
+      when 404
+        raise(ResourceNotFound.new(response))
+      when 405
+        raise(MethodNotAllowed.new(response))
+      when 409
+        raise(ResourceConflict.new(response))
+      when 422
+        raise(ResourceInvalid.new(response))
+      when 401...500
+        raise(ClientError.new(response))
+      when 500...600
+        raise(ServerError.new(response))
+      else
+        raise(ConnectionError.new(response, "Unknown response code: #{response.code}"))
       end
-
-      def request(&block)
-        res = nil
-        Net::HTTP.start(@uri.host, @uri.port) do |http|
-          res = yield(http)
-        end
-        res
-      end
-
-      # Handles response and error codes from remote service.
-      def handle_response(response)
-        case response.code.to_i
-          when 301,302
-            raise(Redirection.new(response))
-          when 200...400
-            response
-          when 400
-            raise(BadRequest.new(response))
-          when 401
-            raise(UnauthorizedAccess.new(response))
-          when 403
-            raise(ForbiddenAccess.new(response))
-          when 404
-            raise(ResourceNotFound.new(response))
-          when 405
-            raise(MethodNotAllowed.new(response))
-          when 409
-            raise(ResourceConflict.new(response))
-          when 422
-            raise(ResourceInvalid.new(response))
-          when 401...500
-            raise(ClientError.new(response))
-          when 500...600
-            raise(ServerError.new(response))
-          else
-            raise(ConnectionError.new(response, "Unknown response code: #{response.code}"))
-        end
-      end
+    end
 
   end
 end
