a840a29dfa1b7fdb9682bb3cd8b6ba30ca3ad358
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-09-20 13:40:02 -0400

    [SPEC] Removed unnecessary code from nested resource fixture

--- spec/support/fixtures/nested_resource.rb
+++ spec/support/fixtures/nested_resource.rb
@@ -1,24 +1,14 @@
 class NestedResource
   include DataMapper::Resource
 
+  storage_names[:default] = %w(parents resources)
+
   property :parent_id,  Integer, :key => true
   property :id,         Serial
 
   property :name,       String
 
-  # Using composite storage name + composite primary key to express the notion of a nested resource
-  class_inheritable_accessor :_resource_name
-  self._resource_name = %w(parents resources)
-
-  def self.storage_name(repository)
-    self._resource_name
-  end
-
   def self.element_name(repository)
     'nested_resource'
-  end
-
-  def self.storage_names
-    { :default => self._resource_name }
   end
 end
